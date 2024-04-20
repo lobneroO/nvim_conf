bb8fe28be83e63d2932fa39cb2677d2c15f1d19e
lobneroO <Tare.noN@web.de>
2024-04-20 14:28:59 +0200

    added <leader>fk to find keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,15 @@
 return {
 	'nvim-telescope/telescope.nvim', branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons',
+	},
 	-- add some key mappings to access the telescope functionalities
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find file"},
 		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
 		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find in buffers"},
 		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags"},
+		{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps"},
 	},
 }
