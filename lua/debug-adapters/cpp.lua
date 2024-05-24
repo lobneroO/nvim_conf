0adab06242f237136a8840d406be153d470ddf67
Tim Lobner <lobner@synopsys.com>
2024-05-24 09:18:01 +0200

    centralized lldb setup for both cpp and rust. changed cpp debugging, which should automagically work now.

--- lua/debug-adapters/cpp.lua
+++ lua/debug-adapters/cpp.lua
@@ -1,39 +1,15 @@
 
 local dap = require('dap')
-if GetOS() == "Unix" then
-    dap.adapters.c_dbg = {
-        type = "executable",
-        command = "gdb",
-        args = { "-i", "dap" }
-    }
-elseif GetOS() == "Windows" then
-    dap.adapters.c_dbg = {
-        type = "executable",
-        command = "clang",
-        -- args = { "-i", "dap" }
-    }
-end
-dap.configurations.c = {
-    {
-        name = "Launch",
-        type = "c_dbg",
-        request = "launch",
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = "${workspaceFolder}",
-        stopAtBeginningOfMainSubprogram = false,
-    },
-}
+
 dap.configurations.cpp = {
     {
         name = "Launch",
-        type = "c_dbg",
+        type = "lldb",
         request = "launch",
         program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            return vim.fn.input('Path. to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
         cwd = "${workspaceFolder}",
-        stopAtBeginningOfMainSubprogram = true,
+        stopAtBeginningOfMainSubprogram = false,
     },
 }
