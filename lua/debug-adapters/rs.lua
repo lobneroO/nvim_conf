e8b8ca977d58502a7ce4ab96d963aa89d9201db3
Tim Lobner <lobner@synopsys.com>
2024-05-28 14:23:08 +0200

    removed rust manual setup, replaced with rustaceanvim and manually installed rust-analyzer. works better on windows.

--- lua/debug-adapters/rs.lua
+++ lua/debug-adapters/rs.lua
@@ -1,6 +1,36 @@
 
 local dap = require('dap')
+<<<<<<< HEAD
+=======
+-- dap.adapters.rust_gdb = {
+--     type = "executable",
+--     command = "rust-gdb",
+--     args = { "-i", "dap" }
+-- }
+--
+-- debugging with rust needs to have the port set.
+>>>>>>> 865d062 (removed rust manual setup, replaced with rustaceanvim and manually installed rust-analyzer. works better on windows.)
 
+local cmd_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/"
+if GetOS() == "Windows" then
+    cmd_path = cmd_path .. "codelldb.exe"
+else
+    cmd_path = cmd_path .."codelldb"
+end
+
+dap.adapters.lldb = {
+    type = 'server',
+    host = '127.0.0.1',
+    port = 13000,
+    executable = {
+
+        command = cmd_path,
+        args = {"--port", "13000"},
+
+        -- possibly needed for windows:
+        detached = false,
+    },
+}
 dap.configurations.rust = {
     {
         name = "Launch",
@@ -13,3 +43,8 @@ dap.configurations.rust = {
         stopAtBeginningOfMainSubprogram = false,
     },
 }
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 865d062 (removed rust manual setup, replaced with rustaceanvim and manually installed rust-analyzer. works better on windows.)
