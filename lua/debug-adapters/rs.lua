347a4e1a4a5c62478682969e5a2a0801dde21616
Tim Lobner <lobner@synopsys.com>
2024-05-28 14:26:24 +0200

    removed rust manual setup, replaced with rustaceanvim and manually installed rust-analyzer. works better on windows.

--- lua/debug-adapters/rs.lua
+++ lua/debug-adapters/rs.lua
@@ -8,6 +8,26 @@ local dap = require('dap')
 --
 -- debugging with rust needs to have the port set.
 
+local cmd_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/"
+if GetOS() == "Windows" then
+    cmd_path = cmd_path .. "codelldb.exe"
+else
+    cmd_path = cmd_path .."codelldb"
+end
+
+dap.adapters.lldb = {
+    type = 'server',
+    host = '127.0.0.1',
+    port = 13000,
+    executable = {
+
+        command = cmd_path,
+        args = {"--port", "13000"},
+
+        -- possibly needed for windows:
+        detached = false,
+    },
+}
 dap.configurations.rust = {
     {
         name = "Launch",
