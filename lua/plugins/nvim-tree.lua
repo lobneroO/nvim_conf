20e73ba78959795586e166c650e92c6135acaa77
lobneroO <dev.tl@mailbox.org>
2024-06-11 18:28:55 +0200

    changed keybindings to ctrl+i for focusing the tree if unfocused and alt+b for toggling hidden files

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -24,8 +24,8 @@ return {
 
         local api = require("nvim-tree.api")
         vim.keymap.set('n', "<C-p>", api.tree.toggle, { desc = "Toggle file tree" })
-        vim.keymap.set('n', "<A-p>", api.tree.focus, { desc = "Focus file tree" })
-        vim.keymap.set('n', "<C-b>", api.tree.toggle_hidden_filter, { desc = "Toggle hidden filter" })
+        vim.keymap.set('n', "<C-i>", api.tree.focus, { desc = "Focus file tree" })
+        vim.keymap.set('n', "<A-b>", api.tree.toggle_hidden_filter, { desc = "Toggle hidden filter" })
         vim.keymap.set('n', "<C-f>", api.tree.change_root_to_node, { desc = "Change root to node" })
     end,
 }
