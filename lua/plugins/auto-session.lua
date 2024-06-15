00cf90f7051ef45b21ffc6c667d0bdf0b5088fde
Tim Lobner <dev.tl@mailbox.org>
2024-06-15 11:33:00 +0200

    added auto session plugin for session management. updated plugins (#24)
    Co-authored-by: Tim Lobner <lobner@synopsys.com>

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,21 @@
+return
+{
+		'rmagatti/auto-session',
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_supress_dirs = {
+					"~/",
+					"~/Downloads",
+					"/",
+				},
+
+				-- set keymap for searching a session in telescope
+				vim.keymap.set('n', "<leader>fs", require("auto-session.session-lens").search_session,{
+					noremap = true, desc = "Browse sessions",
+				})
+			})
+
+			vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+		end
+}
