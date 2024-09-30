5e6cfddd07dcd8d08772d43a4260e8de5c33226e
Tim Lobner <dev.tl@mailbox.org>
2024-09-30 09:53:38 +0200

    enhanced tab line, added shortcut to print filepath (#35)
    * improved buffer line, added leader-s-b-index shortcut for buffer focus picking
    
    * added leader-s-f-p to print the current buffer's absolute file path
    
    ---------
    
    Co-authored-by: Tim Lobner <lobner@synopsys.com>

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,4 +1,4 @@
--- contains key remappings for easier usage
+--dcontains key remappings for easier usage
 vim.g.mapleader = " " -- leader key can be used by many plugins for key mappings
 -- default key to open "file browser" is :Ex in normal mode.
 -- remap this to, while in normal mode (-> "n"), to <leader>pv
@@ -7,3 +7,14 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- quit out of a vim search highlight
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+vim.keymap.set("n", "<leader>sfp",
+    function()
+        -- %:p expands to absolute path
+        local fp = vim.fn.expand('%:p')
+        -- nvim_echo interface is a bit clunky. 
+        -- the string to echo has to be a chunk (-> {{<string>, None}})
+        -- in order to not write this output to log, second param is false
+        -- third param is not optional but not needed
+        vim.api.nvim_echo({{fp, 'None'}}, false, {})
+    end,
+    { desc = "show full file path"})
