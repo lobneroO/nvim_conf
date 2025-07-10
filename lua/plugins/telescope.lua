79ef55ff5fbac2cacfdc88ec370967ee2a2b9ab8
Tim Lobner <lobner@synopsys.com>
2025-07-10 09:23:44 +0200

    added grepping with arguments to live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,12 +3,18 @@ return {
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		'nvim-tree/nvim-web-devicons',
+        {
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            -- This will not install any breaking changes.
+            -- For major updates, this must be adjusted manually.
+            version = "^1.0.0",
+        },
 	},
 	-- add some key mappings to access the telescope functionalities
 	config = function()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find file" })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
+		-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find in buffers" })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Tags" })
 		vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Key Maps" })
@@ -27,12 +33,25 @@ return {
                 }
             }
         }
-        require('telescope').load_extension('git_diffs')
-        vim.keymap.set('n', '<leader>dc', 
+        local telescope = require('telescope')
+        telescope.load_extension('git_diffs')
+        vim.keymap.set('n', '<leader>dc',
             function()
                 require('telescope').extensions.git_diffs.diff_commits()
             end,
             { desc = "Diff HEAD against commit" })
+
+        -- enable arguments for the live grep search. this enables, for example,
+        -- searching only files of a certain type:
+        -- (rg) -t h -t cpp "cuda" src
+        -- this searches all .h and .cpp files for "cuda" in the directory "src"
+        telescope.load_extension('live_grep_args')
+		vim.keymap.set('n', '<leader>fg',
+            function()
+                telescope.extensions.live_grep_args.live_grep_args()
+            end, { desc = "Live grep" })
+
+
 	end,
 }
 
