07b2f1c6e37b80c3bb03a9d34b6803940a244b4f
tim <lobner@synopsys.com>
2025-09-04 14:40:20 +0200

    updated plugins, fixed auto session errors

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -2,21 +2,23 @@ return
 {
 		'rmagatti/auto-session',
         lazy = false,
-		config = function()
-			require("auto-session").setup({
-				log_level = "error",
-				auto_session_supress_dirs = {
-					"~/",
-					"~/Downloads",
-					"/",
-				},
 
-				-- set keymap for searching a session in telescope
-				vim.keymap.set('n', "<leader>fs", require("auto-session.session-lens").search_session,{
-					noremap = true, desc = "Browse sessions",
-				})
-			})
-
-			-- vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-		end
+        --- enables autocomplete for opts
+        --- @module "auto-session"
+        --- @type AutoSession.Config
+        opts = {
+            suppressed_dirs = {
+                "~/",
+                "~/Downloads",
+                "/",
+            },
+            session_lens = {
+                picker = "telescope", -- when using nil, it autoselects
+                mappings = {
+                    delete_session = { "i", "<leader>ds"}
+                },
+            },
+            -- log_level = 'debug',
+        },
+        load_on_setup = true,
 }
