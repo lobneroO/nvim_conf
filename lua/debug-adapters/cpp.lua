51e686a95f74c65a95b5830c2f5d7e4c7cadcdb2
tim <lobner@synopsys.com>
2025-09-19 15:49:21 +0200

    experimental cppvsdbg support on windows

--- lua/debug-adapters/cpp.lua
+++ lua/debug-adapters/cpp.lua
@@ -1,15 +1,19 @@
-
 local dap = require('dap')
+
+local function is_windows()
+    return GetOS and GetOS() == "Windows"
+end
 
 dap.configurations.cpp = {
     {
         name = "Launch",
-        type = "lldb",
+        type = is_windows() and "cppvsdbg" or "lldb",
         request = "launch",
         program = function()
-            return vim.fn.input('Path. to executable: ', vim.fn.getcwd() .. '/', 'file')
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
         cwd = "${workspaceFolder}",
-        stopAtBeginningOfMainSubprogram = false,
+        stopOnEntry = true,
+        args = {},
     },
 }
--- lua/debug-adapters/lldb_setup.lua
+++ lua/debug-adapters/lldb_setup.lua
@@ -1,8 +1,12 @@
-
 local dap = require('dap')
 
+local function is_windows()
+    return GetOS and GetOS() == "Windows"
+end
+
+-- LLDB setup
 local cmd_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/"
-if GetOS() == "Windows" then
+if is_windows() then
     cmd_path = cmd_path .. "codelldb.exe"
 else
     cmd_path = cmd_path .."codelldb"
@@ -13,11 +17,35 @@ dap.adapters.lldb = {
     host = '127.0.0.1',
     port = 13000,
     executable = {
-
         command = cmd_path,
         args = {"--port", "13000"},
-
-        -- possibly needed for windows:
         detached = false,
     },
 }
+
+-- cppvsdbg setup for Windows
+if is_windows() then
+    dap.adapters.cppvsdbg = {
+        id = 'cppvsdbg',
+        type = 'executable',
+        command = 'C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/Extensions/Microsoft/Debugger/cppvsdbg.exe',
+        options = {
+            detached = false
+        }
+    }
+end
+
+-- Example configuration for C++
+-- dap.configurations.cpp = {
+--     {
+--         name = "Launch file",
+--         type = is_windows() and "cppvsdbg" or "lldb",
+--         request = "launch",
+--         program = function()
+--             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--         end,
+--         cwd = '${workspaceFolder}',
+--         stopOnEntry = true,
+--         args = {},
+--     },
+-- }
