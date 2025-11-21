d908a97de63e911e3eb9a715f065fcaee779d475
Tim Lobner <tim.lobner@keysight.com>
2025-11-21 16:27:20 +0100

    added surround for easier surround action

---
+++ lua/plugins/mini.surround.lua
@@ -0,0 +1,16 @@
+-- enables surrounding a word (or motion, really) with characters,
+-- e.g. have
+--  my word
+-- hover cursor at the 'y', use sa2w) (i.e: [s]urround [a]dd [2w]ords parantheses, closing such that no space is added)
+-- and it will turn into 
+--  (my word)
+-- in contrast, using sa2w( will add spaces around the parantheses
+--  ( my word )
+return {
+    'nvim-mini/mini.surround',
+    version = '*',
+
+    config = function()
+        require('mini.surround').setup()
+    end
+}
