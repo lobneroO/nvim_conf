3845671613cbbab00d64f086c432c09ea9898a54
Tim Lobner <tim.lobner@keysight.com>
2025-12-15 11:28:01 +0100

    fix folding in some cases where neovim would throw errors on every interaction

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -12,6 +12,20 @@ return {
 		vim.o.foldenable = true
 
 		-- setup folding source: first lsp, then indent as fallback
+        -- Tell the server the capability of foldingRange,
+        -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+        }
+        local language_servers = vim.lsp.get_clients()
+        for _, ls in ipairs(language_servers) do
+            require('lspconfig')[ls].setup({
+                capabilities = capabilities
+                -- you can add other fields for setting up lsp server in this table
+            })
+        end
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
 				return { 'lsp', 'indent' }
