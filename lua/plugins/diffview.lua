dcf9c96a30317c23174c5f8c6eebc85c37cd937a
Tim Lobner <tim.lobner@keysight.com>
2026-03-04 09:50:50 +0100

    replaced unmaintained upstream diffview with actively maintained fork

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,7 +1,9 @@
 -- install plugin for diff and merge views
+-- this is a fork of the original diffview, as that seems to not be maintained anylonger.
+-- original at https://github.com/sindrets/diffview.nvim
 
 return {
-	"sindrets/diffview.nvim",
+	"dlyongemallo/diffview.nvim",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
@@ -19,8 +21,33 @@ return {
 					vim.cmd('DiffviewClose')
 				end
 			end,
-			{ desc = "Toggle diffview on/off"}
+			{ desc = "Toggle diffview on/off" }
 		)
+
+        -- file history (no toggle available)
+        vim.keymap.set('n',
+            '<leader>dh', '<cmd>DiffviewFileHistory %<cr>',
+            { desc = "Open file history for current file" }
+        )
+        vim.keymap.set('n',
+            '<leader>dH', '<cmd>DiffviewFileHistory<cr>',
+            { desc = "Open file history for the entire repo" }
+        )
+        -- file history for selection
+        vim.keymap.set('n',
+            '<leader>dh', "<Esc><cmd>'<,'>DiffviewFileHistory --follow<cr>",
+            { desc = "Open file history for selection" }
+        )
+        
+        -- diff against main/master
+        vim.keymap.set('n', '<leader>dm', function()
+            -- try main first, fall back to master
+            local handle = io.popen('git rev-parse --verify main 2>/dev/null')
+            local result = handle:read('*a')
+            handle:close()
+            local branch = result ~= '' and 'main' or 'master'
+            vim.cmd('Diffview Open' .. branch)
+        end, { desc = "Diff against main/master" })
 	end,
 }
 
--- lua/plugins/telescope-git-diffs.lua
+++ lua/plugins/telescope-git-diffs.lua
@@ -2,6 +2,6 @@ return {
     'paopaol/telescope-git-diffs.nvim',
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim",
+        "dlyongemallo/diffview.nvim",
     },
 }
